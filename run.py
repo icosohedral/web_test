5591278c4f390956d064ababe5af94b4398aa4fd
icosohedral <catcher.in.the.rye.v@gmail.com>
2023-12-25 14:34:11 +0800

    2

--- run.py
+++ run.py
@@ -3,15 +3,21 @@
 import os
 import sys
 
-def main():
+def main(https=False):
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
     try:
         from django.core.management import execute_from_command_line
     except:
         raise ImportError("Couldn't import Django.")
     script_dir = os.path.dirname(os.path.realpath(__file__)) + '/'
-    argv = [sys.argv[0], "runserver_plus", '0.0.0.0:443', '--cert', '%sserver.crt' % script_dir, '--key', '%sserver.key' % script_dir]
+    if https:
+        argv = [sys.argv[0], "runserver_plus", '0.0.0.0:443', '--cert', '%sserver.crt' % script_dir, '--key', '%sserver.key' % script_dir]
+    else:
+        argv = [sys.argv[0], "runserver_plus", '0.0.0.0:8000']
     execute_from_command_line(argv)
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 1:
+        main(https=True)
+    else:
+        main()
